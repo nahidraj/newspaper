a6fc04d7ee916c6edf58032fa8d1244c909ec664
nahid12 <nahidraj.web@gmail.com>
2023-11-26 23:26:45 +0600

    all pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -170,12 +170,12 @@
 		$(this).find(".right").toggleClass("fa-caret-up fa-caret-down");
 	});
 
-	document.addEventListener('contextmenu', function (e) {
-		e.preventDefault();
-	});
-	document.addEventListener('keydown', function (e) {
-		if (e.ctrlKey && e.key === 'u') {
-			e.preventDefault();
-		}
-	});
+	// document.addEventListener('contextmenu', function (e) {
+	// 	e.preventDefault();
+	// });
+	// document.addEventListener('keydown', function (e) {
+	// 	if (e.ctrlKey && e.key === 'u') {
+	// 		e.preventDefault();
+	// 	}
+	// });
 })(jQuery);
